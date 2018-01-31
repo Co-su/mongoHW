7ace91703bfe80a20b6db826d9924e60bf32aa59
Co-Su <thisusernameisnottaken@gmail.com>
2018-01-31 12:33:15 -0800

    basic functionality nearly established.  need to fix note errors.

---
+++ public/app.js
@@ -0,0 +1,55 @@
+$.getJSON("/articles", function(data) {
+  for (var i = 0; i < data.length; i++) {
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<h3>" + data[i].title + "</h3>" + "<br />" + "<a href = 'https://www.fantasyflightgames.com" + data[i].link + "'>Read more...</a><br /></p>");
+  }
+});
+
+
+$(document).on("click", "p", function() {
+  console.log("yo");
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    .then(function(data) {
+      console.log(data);
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      if (data.note) {
+        $("#titleinput").val(data.note.title);
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+
+    .then(function(data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
