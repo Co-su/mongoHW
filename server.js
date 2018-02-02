14640e7add1f83c525bae45348f424cffa56e02b
Co-Su <thisusernameisnottaken@gmail.com>
2018-02-02 11:49:34 -0800

    maybe fixed heroku issue?

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var cool = require('cool-ascii-faces');
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var $ = require("jquery");
+
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
@@ -12,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
